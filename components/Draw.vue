49d36c97d9c3c263fadd86358e6fe4fdbb43e4c8
Gabriel Osuobiem <osuobiem@gmail.com>
2020-02-13 01:05:58 +0100

    Add Draw component to show weekly draw

---
+++ components/Draw.vue
@@ -0,0 +1,89 @@
+<template>
+  <!-- lottery ticket begin -->
+  <div class="lottery-ticket" id="lottery-ticket">
+    <div class="container">
+      <div
+        class="row justify-content-xl-between justify-content-lg-between justify-content-md-center"
+      >
+        <div class="col-xl-5 col-lg-4">
+          <div class="part-img">
+            <div class="ill-width">
+              <div class="ill-shape-set">
+                <img src="~/assets/img/svg/lottery-jackpot-icon.png" alt="" />
+                <img src="~/assets/img/svg/lottery-jackpot-icon.png" alt="" />
+                <img src="~/assets/img/svg/lottery-jackpot-icon.png" alt="" />
+                <img src="~/assets/img/svg/lottery-jackpot-icon.png" alt="" />
+              </div>
+            </div>
+            <img src="~/assets/img/lottery-jackpot-img.png" alt="" />
+            <div class="draw-date">
+              <span class="title">Draw</span>
+              <span class="date">26 Dec</span>
+            </div>
+            <div class="clock">
+              <img src="~/assets/img/clock.webp" alt="" />
+            </div>
+          </div>
+        </div>
+
+        <div class="col-xl-6 col-lg-7 col-md-9">
+          <div class="part-text">
+            <h2>Buy lottery tickets online</h2>
+            <p>
+              Lotto tickets online are a great alternative to paper tickets; you
+              don’t have to visit a local lottery office any more. You don’t
+              have to wait in line to purchase a lottery ticket online. Play a
+              lottery online using the possibilities of modern technology and
+              the expansion of the internet.
+            </p>
+            <p>
+              Check out the lotteries available in our offer. Pay close
+              attention to the time remaining for each draw, as well as to the
+              jackpot prize amount!
+            </p>
+            <div
+              class="date-counter timer"
+              data-date="30 November 2019 9:56:00 GMT+01:00"
+            >
+              <div class="single-counter">
+                <div class="conter-content">
+                  <span class="number day">26</span>
+                  <span class="title">Days</span>
+                </div>
+              </div>
+              <div class="single-counter">
+                <div class="conter-content">
+                  <span class="number hour">15</span>
+                  <span class="title">Hour</span>
+                </div>
+              </div>
+              <div class="single-counter">
+                <div class="conter-content">
+                  <span class="number minute">25</span>
+                  <span class="title">Minutes</span>
+                </div>
+              </div>
+              <div class="single-counter">
+                <div class="conter-content">
+                  <span class="number second">35</span>
+                  <span class="title">Seconds</span>
+                </div>
+              </div>
+            </div>
+            <div class="buttons">
+              <ul>
+                <li>
+                  <a href="#">Register & Play</a>
+                </li>
+                <li>
+                  <a href="#">View All Offer</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- lottery ticket end -->
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,59 +1,68 @@
 <template>
-  <!-- banner begin -->
-  <div class="banner">
-    <div class="container">
-      <div class="row justify-content-center">
-        <div class="col-xl-6 col-lg-9 col-md-8">
-          <div class="banner-content">
-            <h1>
-              Play Now,<br />
-              Laugh Forever!
-            </h1>
-            <p>
-              Give yourself the gift of a dream.<br />Buy a ticket today and
-              stand a chance to win big.<br />
-              <strong>Give us the opportunity to change your life.</strong>
-            </p>
-            <a href="#" class="banner-button">Play Now</a>
+  <div>
+    <!-- banner begin -->
+    <div class="banner">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-xl-6 col-lg-9 col-md-8">
+            <div class="banner-content">
+              <h1>
+                Play Now,<br />
+                Laugh Forever!
+              </h1>
+              <p>
+                Give yourself the gift of a dream.<br />Buy a ticket today and
+                stand a chance to win big.<br />
+                <strong>Give us the opportunity to change your life.</strong>
+              </p>
+              <a href="#" class="banner-button">Play Now</a>
+            </div>
           </div>
-        </div>
-        <div class="col-xl-6 col-lg-6 d-xl-flex d-xl-flex align-items-center">
-          <div class="part-img">
-            <img src="~/assets/img/banner-img.png" alt="" />
-            <div class="ill-shape-set">
-              <img src="~/assets/img/svg/ill-1.png" alt="" />
-              <img src="~/assets/img/svg/ill-2.png" alt="" />
-              <img src="~/assets/img/svg/ill-3.png" alt="" />
-              <img src="~/assets/img/svg/ill-4.png" alt="" />
-            </div>
-            <div class="ill-shape-set-2">
-              <img src="~/assets/img/svg/ill-1.png" alt="" />
-              <img src="~/assets/img/svg/ill-2.png" alt="" />
-              <img src="~/assets/img/svg/ill-3.png" alt="" />
-              <img src="~/assets/img/svg/ill-4.png" alt="" />
-            </div>
-            <div class="ill-shape-1">
-              <img src="~/assets/img/svg/ill-5.png" alt="" />
-            </div>
-            <div class="ill-shape-2">
-              <img src="~/assets/img/svg/ill-5.png" alt="" />
-            </div>
-            <div class="ill-shape-3">
-              <img src="~/assets/img/svg/ill-5.png" alt="" />
-            </div>
-            <div class="ill-shape-4">
-              <img src="~/assets/img/svg/ill-5.png" alt="" />
+          <div class="col-xl-6 col-lg-6 d-xl-flex d-xl-flex align-items-center">
+            <div class="part-img">
+              <img src="~/assets/img/banner-img.png" alt="" />
+              <div class="ill-shape-set">
+                <img src="~/assets/img/svg/ill-1.png" alt="" />
+                <img src="~/assets/img/svg/ill-2.png" alt="" />
+                <img src="~/assets/img/svg/ill-3.png" alt="" />
+                <img src="~/assets/img/svg/ill-4.png" alt="" />
+              </div>
+              <div class="ill-shape-set-2">
+                <img src="~/assets/img/svg/ill-1.png" alt="" />
+                <img src="~/assets/img/svg/ill-2.png" alt="" />
+                <img src="~/assets/img/svg/ill-3.png" alt="" />
+                <img src="~/assets/img/svg/ill-4.png" alt="" />
+              </div>
+              <div class="ill-shape-1">
+                <img src="~/assets/img/svg/ill-5.png" alt="" />
+              </div>
+              <div class="ill-shape-2">
+                <img src="~/assets/img/svg/ill-5.png" alt="" />
+              </div>
+              <div class="ill-shape-3">
+                <img src="~/assets/img/svg/ill-5.png" alt="" />
+              </div>
+              <div class="ill-shape-4">
+                <img src="~/assets/img/svg/ill-5.png" alt="" />
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
+    <!-- banner end -->
+
+    <Draw />
   </div>
-  <!-- banner end -->
 </template>
 
 <script>
+import Draw from '@/components/Draw'
+
 export default {
+  components: {
+    Draw
+  },
   layout: 'main-lay'
 }
 </script>
