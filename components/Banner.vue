95ec07e97bdbec25816accee9cbe79fbf0955aa7
Gabriel Osuobiem <osuobiem@gmail.com>
2020-02-14 15:38:06 +0100

    Add breadcrumb component

---
+++ components/Banner.vue
@@ -0,0 +1,27 @@
+<template>
+  <!-- breadcrumb begin -->
+  <div class="breadcrumb-lotters">
+    <div class="banner-shape">
+      <img src="~/assets/img/svg/banner-shape-1.png" alt="" />
+      <img src="~/assets/img/svg/banner-shape-2.png" alt="" />
+    </div>
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-xl-6 col-lg-6">
+          <div class="breadcrumb-content">
+            <h2>{{ page }}</h2>
+            <ul>
+              <li>
+                <nuxt-link to="/">Home</nuxt-link>
+              </li>
+              <li>
+                {{ detail }}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- breadcrumb end -->
+</template>
