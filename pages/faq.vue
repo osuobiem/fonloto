12e498f632ac5e2935f1bce74db550baa698887d
Gabriel Osuobiem <osuobiem@gmail.com>
2020-02-14 15:44:20 +0100

    Add banner to faq page

---
+++ pages/faq.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Banner :page="page" :detail="detail" />
+  </div>
+</template>
+
+<script>
+import Banner from '@/components/Banner'
+
+export default {
+  data() {
+    return {
+      page: 'FAQ',
+      detail: 'Frequently Asked Questions'
+    }
+  },
+  components: {
+    Banner
+  },
+  layout: 'alt-lay'
+}
+</script>
