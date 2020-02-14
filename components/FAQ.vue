60daff3d271960697af20c4207d410e9a4366a57
Gabriel Osuobiem <osuobiem@gmail.com>
2020-02-14 16:20:31 +0100

    Add FAQ component

---
+++ components/FAQ.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="col-xl-8 col-lg-8">
+                        <div class="faq-content">
+                            <div class="tab-content" id="v-pills-tabContent">
+                                <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
+                                    <div class="single-faq">
+                                        <h4>Can I participate in a national lottery of a foreign country?</h4>
+                                        <p>It is a long established fact that a reader will be distracted by the readable content of a page whene at
+                                            layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters too
+                                            using eader will be distracted by the readable conten.</p>
+                                    </div>
+
+                                    <div class="single-faq">
+                                        <h4>How old do I need to be to play the lottery?</h4>
+                                        <p>It is a long established fact that a reader will be distracted by the readable content of a page whene at
+                                            layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters too
+                                            using eader will be distracted by the readable conten.</p>
+                                    </div>
+
+                                    <div class="single-faq">
+                                        <h4>What is the life cycle of my entry?</h4>
+                                        <p>It is a long established fact that a reader will be distracted by the readable content of a page whene at
+                                            layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters too
+                                            using eader will be distracted by the readable conten.</p>
+                                    </div>
+                                </div>
+
+                                <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">
+                                    <div class="single-faq">
+                                        <h4>What is the deadline for buying a ticket for the drawing?</h4>
+                                        <p>It is a long established fact that a reader will be distracted by the readable content of a page whene at
+                                            layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters too
+                                            using eader will be distracted by the readable conten.</p>
+                                    </div>
+                                    <div class="single-faq">
+                                        <h4>How many tickets can I buy for one draw in each lottery?</h4>
+                                        <p>It is a long established fact that a reader will be distracted by the readable content of a page whene at
+                                            layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters too
+                                            using eader will be distracted by the readable conten.</p>
+                                    </div>
+                                    <div class="single-faq">
+                                        <h4>Can I play if I don’t have a bank card?</h4>
+                                        <p>It is a long established fact that a reader will be distracted by the readable content of a page whene at
+                                            layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters too
+                                            using eader will be distracted by the readable conten.</p>
+                                    </div>
+                                </div>
+                                
+                                <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">
+                                    <div class="single-faq">
+                                        <h4>When are the results published?</h4>
+                                        <p>It is a long established fact that a reader will be distracted by the readable content of a page whene at
+                                            layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters too
+                                            using eader will be distracted by the readable conten.</p>
+                                    </div>
+
+                                    <div class="single-faq">
+                                        <h4>How do I check if I’ve won?</h4>
+                                        <p>It is a long established fact that a reader will be distracted by the readable content of a page whene at
+                                            layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters too
+                                            using eader will be distracted by the readable conten.</p>
+                                    </div>
+
+                                    <div class="single-faq">
+                                        <h4>Is there automatic notification of winnings?</h4>
+                                        <p>It is a long established fact that a reader will be distracted by the readable content of a page whene at
+                                            layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters too
+                                            using eader will be distracted by the readable conten.</p>
+                                    </div>
+                                </div>
+                                <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">
+                                    <div class="single-faq">
+                                        <h4>Do I need to register to play?</h4>
+                                        <p>It is a long established fact that a reader will be distracted by the readable content of a page whene at
+                                            layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters too
+                                            using eader will be distracted by the readable conten.</p>
+                                    </div>
+
+                                    <div class="single-faq">
+                                        <h4>How do I register?</h4>
+                                        <p>It is a long established fact that a reader will be distracted by the readable content of a page whene at
+                                            layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters too
+                                            using eader will be distracted by the readable conten.</p>
+                                    </div>
+
+                                    <div class="single-faq">
+                                        <h4>Do I need to confirm my email address?</h4>
+                                        <p>It is a long established fact that a reader will be distracted by the readable content of a page whene at
+                                            layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters too
+                                            using eader will be distracted by the readable conten.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+</template>
