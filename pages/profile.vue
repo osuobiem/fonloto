7201abf32a28845bc9b4412caf8a18393534b171
Gabriel Osuobiem <osuobiem@gmail.com>
2020-02-16 19:55:14 +0100

    Create profile page and children folder

---
+++ pages/profile.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <!-- profile begin -->
+    <div class="profile">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-xl-3 col-lg-3">
+            <div class="accont-tab">
+              <div
+                class="nav flex-column nav-pills"
+                id="v-pills-tab"
+                role="tablist"
+                aria-orientation="vertical"
+              >
+                <a
+                  class="nav-link active"
+                  id="personal-info-tab"
+                  data-toggle="pill"
+                  href="#personal-info"
+                  role="tab"
+                  aria-selected="true"
+                  ><i class="far fa-user"></i> Personal Info</a
+                >
+                <a
+                  class="nav-link"
+                  id="payment-info-tab"
+                  data-toggle="pill"
+                  href="#payment-info"
+                  role="tab"
+                  aria-selected="false"
+                  ><i class="fas fa-credit-card"></i> Payment Info</a
+                >
+                <a
+                  class="nav-link"
+                  id="v-pills-messages-tab"
+                  data-toggle="pill"
+                  href="#v-pills-messages"
+                  role="tab"
+                  aria-controls="v-pills-messages"
+                  aria-selected="false"
+                  ><i class="fas fa-tasks"></i> Privacy Settings</a
+                >
+                <a
+                  class="nav-link"
+                  id="v-pills-settings-tab"
+                  data-toggle="pill"
+                  href="#v-pills-settings"
+                  role="tab"
+                  aria-controls="v-pills-settings"
+                  aria-selected="false"
+                  ><i class="fas fa-cog"></i> Account Settings</a
+                >
+              </div>
+            </div>
+          </div>
+          <div class="col-xl-8 col-lg-8">
+            <div class="tab-content" id="v-pills-tabContent">
+              <!-- Child Page -->
+              <nuxt-child />
+              <!-- End Child Page -->
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- profile end -->
+  </div>
+</template>
+
+<script>
+import Banner from '@/components/Banner'
+
+export default {
+  layout: 'alt-lay'
+}
+</script>
--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -1,14 +1,93 @@
 <template>
-  <Profile />
+  <div>
+    <div
+      class="tab-pane fade show active"
+      id="personal-info"
+      role="tabpanel"
+      aria-labelledby="personal-info-tab"
+    >
+      <div class="user-panel-title">
+        <h3>Brian's Profile</h3>
+      </div>
+      <div class="player-profile">
+        <div class="row no-gutters">
+          <div class="col-xl-4 col-lg-4 col-md-6">
+            <div class="player-card">
+              <div class="part-pic">
+                <img src="~/assets/img/player-user.jpg" alt="" />
+              </div>
+              <div class="social-link">
+                <ul>
+                  <li>
+                    <a href="#">
+                      <i class="fab fa-facebook-messenger"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i class="fab fa-youtube"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i class="fab fa-twitter"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="col-xl-8 col-lg-8 col-md-6">
+            <div class="part-info">
+              <span class="player-name">
+                Brian Summerhoold
+                <img src="~/assets/img/svg/medal.svg" alt="" />
+              </span>
+              <ul>
+                <li>
+                  <span class="property"
+                    ><i class="fas fa-info-circle"></i> First Name :</span
+                  >
+                  <span class="value">Brian John </span>
+                </li>
+                <li>
+                  <span class="property"
+                    ><i class="fas fa-info-circle"></i> Last Name :</span
+                  >
+                  <span class="value">Summerhoold N</span>
+                </li>
+                <li>
+                  <span class="property"
+                    ><i class="fas fa-info-circle"></i> Email Address :</span
+                  >
+                  <span class="value">autworks2018@gmail.com</span>
+                </li>
+                <li>
+                  <span class="property"
+                    ><i class="fas fa-info-circle"></i> Phone Numbers :</span
+                  >
+                  <span class="value">+88 015 255 4365</span>
+                </li>
+                <li>
+                  <span class="property"
+                    ><i class="fas fa-info-circle"></i> Website :</span
+                  >
+                  <span class="value">http://themeforest.net</span>
+                </li>
+                <li>
+                  <span class="property"
+                    ><i class="fas fa-info-circle"></i> State :</span
+                  >
+                  <span class="value">United Kingdom</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="edit-profile text-center">
+        <a href="#" class="edit-button">Edit Profile</a>
+      </div>
+    </div>
+  </div>
 </template>
-
-<script>
-import Profile from '@/components/Profile'
-
-export default {
-  components: {
-    Profile
-  },
-  layout: 'alt-lay'
-}
-</script>
