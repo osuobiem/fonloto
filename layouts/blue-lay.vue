f10bddbecb1392fbbca83ed9091d458b7dc28ed6
Gabriel Osuobiem <osuobiem@gmail.com>
2020-02-16 19:56:57 +0100

    Add blue layer

---
+++ layouts/blue-lay.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <AppHeader />
+
+    <nuxt />
+
+    <AppFooter :links="links" />
+    <Copyright />
+    <MobileNav />
+  </div>
+</template>
+
+<script>
+import AppHeader from '@/components/Blue-Header'
+import MobileNav from '@/components/Mobile-Nav'
+import Copyright from '@/components/Copyright'
+import AppFooter from '@/components/Footer'
+
+export default {
+  data() {
+    return {
+      links: [
+        { name: 'Home', nuxt: true, url: '/' },
+        { name: 'Draws', nuxt: false, url: '/draws' },
+        { name: 'About Us', nuxt: true, url: '/about-us' },
+        { name: 'F.A.Q', nuxt: true, url: '/faq' },
+        { name: 'Contact Us', nuxt: true, url: '/contact' },
+        { name: 'Deposit', nuxt: false, url: '/deposit' }
+      ]
+    }
+  },
+  components: {
+    AppHeader,
+    MobileNav,
+    Copyright,
+    AppFooter
+  }
+}
+</script>
+
+<style>
+@import url('~/assets/css/style.css');
+@import url('~/assets/css/bootstrap.min.css');
+@import url('~/assets/css/animate.css');
+@import url('~/assets/css/responsive.css');
+@import url('~/assets/css/jquery-jvectormap-2.0.3.css');
+@import url('~/assets/css/simple-scrollbar.css');
+</style>
