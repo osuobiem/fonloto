842f4de402cfffc5981a598083d82a76bb3074f0
Gabriel Osuobiem <osuobiem@gmail.com>
2020-02-16 04:18:27 +0100

    Move timer code from main js to page

--- pages/index.vue
+++ pages/index.vue
@@ -213,6 +213,52 @@ export default {
     Draw,
     Contact
   },
-  layout: 'main-lay'
+  layout: 'main-lay',
+  mounted() {
+    var nodes = $('.timer')
+    $.each(nodes, function(_index, value) {
+      var date = $(this).data('date')
+
+      setInterval(() => {
+        var endTime = new Date(date)
+        endTime = Date.parse(endTime) / 1000
+
+        var now = new Date()
+        now = Date.parse(now) / 1000
+
+        var timeLeft = endTime - now
+
+        var days = Math.floor(timeLeft / 86400)
+        var hours = Math.floor((timeLeft - days * 86400) / 3600)
+        var minutes = Math.floor((timeLeft - days * 86400 - hours * 3600) / 60)
+        var seconds = Math.floor(
+          timeLeft - days * 86400 - hours * 3600 - minutes * 60
+        )
+
+        if (hours < '10') {
+          hours = '0' + hours
+        }
+        if (minutes < '10') {
+          minutes = '0' + minutes
+        }
+        if (seconds < '10') {
+          seconds = '0' + seconds
+        }
+
+        $(value)
+          .find('.day')
+          .html(days)
+        $(value)
+          .find('.hour')
+          .html(hours)
+        $(value)
+          .find('.minute')
+          .html(minutes)
+        $(value)
+          .find('.second')
+          .html(seconds)
+      }, 1000)
+    })
+  }
 }
 </script>
