97d4fec97ab80f01dbc8efa376e8fc8e391ca670
Gabriel Osuobiem <osuobiem@gmail.com>
2020-02-21 12:21:32 +0100

    Add latest winners component

---
+++ components/Latest-Winners.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="col-xl-4 col-lg-8">
+    <div class="winners-list">
+      <h3>Latest Winners</h3>
+      <div class="row">
+        <div class="col-xl-12 col-lg-6">
+          <div class="Vertical-Slider">
+            <div class="single-winner mb-xl-2 mb-lg-0">
+              <div class="part-img">
+                <img src="~/assets/img/flag-1.jpg" class="flag" alt="" />
+              </div>
+              <div class="part-text">
+                <span class="name">Mukta Marten</span>
+                <span class="lottery-subject">Mega Millions / 30-10-2019</span>
+                <span class="winning-prize">$299</span>
+              </div>
+            </div>
+            <div class="single-winner">
+              <div class="part-img">
+                <img src="~/assets/img/flag-2.jpg" class="flag" alt="" />
+              </div>
+              <div class="part-text">
+                <span class="name">John Tamim</span>
+                <span class="lottery-subject">Mega Millions / 30-10-2019</span>
+                <span class="winning-prize">$568</span>
+              </div>
+            </div>
+            <div class="single-winner mb-xl-2 mb-lg-0">
+              <div class="part-img">
+                <img src="~/assets/img/flag-3.jpg" class="flag" alt="" />
+              </div>
+              <div class="part-text">
+                <span class="name">Mukta Marten</span>
+                <span class="lottery-subject">Mega Millions / 30-10-2019</span>
+                <span class="winning-prize">$126</span>
+              </div>
+            </div>
+            <div class="single-winner">
+              <div class="part-img">
+                <img src="~/assets/img/flag-4.jpg" class="flag" alt="" />
+              </div>
+              <div class="part-text">
+                <span class="name">John Tamim</span>
+                <span class="lottery-subject">Mega Millions / 30-10-2019</span>
+                <span class="winning-prize">$974</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
