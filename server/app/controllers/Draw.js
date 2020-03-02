589c7a6b2c3bd771cbaca6f8e53beb03166c4df1
Gabriel Osuobiem <osuobiem@gmail.com>
2020-03-02 11:01:31 +0100

    Draw controller update() modified

--- server/app/controllers/Draw.js
+++ server/app/controllers/Draw.js
@@ -32,8 +32,31 @@ module.exports = {
   },
 
   async update(data, criteria) {
-    draw.attr = data
-    return await draw.update(criteria)
+    return new Promise((resolve, reject) => {
+      this.get(criteria).then(res => {
+        if (res.length < 1) {
+          reject('Draw does not exist!')
+        } else {
+          draw.attr = data
+          draw
+            .update(criteria)
+            .then(() => {
+              this.get(criteria)
+                .then(resp => {
+                  resolve(resp[0])
+                })
+                .catch(er => {
+                  console.log(er)
+                  reject('Opps! Could not update draw')
+                })
+            })
+            .catch(err => {
+              console.log(err)
+              reject('Opps! Could not update draw')
+            })
+        }
+      })
+    })
   },
 
   async delete(criteria = {}) {
--- server/router.js
+++ server/router.js
@@ -147,14 +147,13 @@ const Draw = require('./app/controllers/Draw')
   // Update
   app.post('/draws/update/:id', (req, res) => {
     validate.empty(req.body)
-
     if (validate.status) {
-      Draw.update(req.body, { id: parseInt(req.params.id) })
-        .then(data => {
-          report.success(res, data[0])
+      Draw.update(req.body, { id: req.params.id })
+        .then(resp => {
+          report.success(res, resp)
         })
         .catch(err => {
-          report.failure(res, 'Could not update data', err)
+          report.failure(res, err)
         })
     } else {
       report.failure(res, validate.report())
