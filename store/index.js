59006ae4b69ed1302600466fb1367dee146248da
Gabriel Osuobiem <osuobiem@gmail.com>
2020-04-02 01:27:04 +0100

    Added faq vuex module

--- store/index.js
+++ store/index.js
@@ -3,13 +3,15 @@ import vuex from 'vuex'
 import site from './modules/site'
 import draws from './modules/draws'
 import contact from './modules/contact'
+import faq from './modules/faq'
 
 let createStore = () => {
   return new vuex.Store({
     modules: {
       site,
       draws,
-      contact
+      contact,
+      faq
     }
   })
 }
---
+++ store/modules/faq.js
@@ -0,0 +1,70 @@
+export default {
+  state: {
+    faqs: {},
+    categories: []
+  },
+
+  getters: {
+    faq: (state, id) => state.draws[id]
+  },
+
+  mutations: {
+    SET_FAQS(state, data) {
+      let cat_id = data.cat_id
+      state.faqs[cat_id] = data.faqs
+    },
+    SET_CATS(state, data) {
+      state.categories = data
+    }
+  },
+
+  actions: {
+    nuxtServerInit: {
+      root: true,
+      handler: context => {
+        return new Promise((resolve, reject) => {
+          context
+            .dispatch('getCategories')
+            .then(data => {
+              context.commit('SET_CATS', data)
+              data.forEach(cat => {
+                let id = cat.id
+                context
+                  .dispatch('getFaqs', id)
+                  .then(data => {
+                    let f_data = {
+                      cat_id: id,
+                      faqs: data
+                    }
+                    context.commit('SET_FAQS', f_data)
+                  })
+                  .catch(err => {
+                    console.log(err)
+                    reject(err)
+                  })
+              })
+              resolve()
+            })
+            .catch(err => {
+              console.log(err)
+              reject(err)
+            })
+        })
+      }
+    },
+    async getFaqs(context, category) {
+      let faqs = await this.$axios.get(
+        process.env.BASE_URL + '/api/faqs/category/' + category
+      )
+      faqs = faqs.data.data
+
+      return faqs
+    },
+    async getCategories() {
+      let cats = await this.$axios.get(process.env.BASE_URL + '/faq-cats')
+      cats = cats.data.data
+
+      return cats
+    }
+  }
+}
